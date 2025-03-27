dd9514f1f04f049c9da2dadbc43699210dc330ca
William Convertino <williamconvertino@gmail.com>
2025-03-27 15:09:38 -0400

    Fixed model name

--- core/lightning.py
+++ core/lightning.py
@@ -66,7 +66,7 @@ class ModelLightningWrapper(pl.LightningModule):
         self.learning_rate = learning_rate
         self.start_datetime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         
-        self.log_dir = os.path.join("logs", self.model.name)
+        self.log_dir = os.path.join("logs", self.model.config.name)
         os.makedirs(self.log_dir, exist_ok=True)
         self.log_file = os.path.join(self.log_dir, f"{self.start_datetime}.log")
         
